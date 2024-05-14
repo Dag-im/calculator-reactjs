6f86e27655288dfc29478f253f11d5291652e241
Dagim Meseret <105111996+Dag-im@users.noreply.github.com>
2024-05-14 20:45:25 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calculator from './components/Calculator'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Calculator />
+      <p className='developer'>Developed by Dagim Meseret UGR/1398/14</p>
+    </div>
+  )
+}
+
+export default App
